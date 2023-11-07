2ed124cce940f86253abebe995393c3126bd48dc
KaijuGirlFanboy <150186749+KaijuGirlFanboy@users.noreply.github.com>
2023-11-07 14:49:22 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("Indeed")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showString("Of course my dear lady, you are truly an idiotic entity")
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("Truly")
+})
+basic.forever(function () {
+	
+})
